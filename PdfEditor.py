a08567d3003bced4fed8c07011aff98d1d871f4d
LEE YONGJOO <djm07048@gmail.com>
2024-08-11 23:14:07 +0900

    240811

--- PdfEditor.py
+++ PdfEditor.py
@@ -15,3 +15,19 @@ class PdfOverlay:
     def save_pdf(self, output_path):
         with open(output_path, 'wb') as f:
             self.writer.write(f)
+
+class PdfOverlay:
+    def __init__(self, pdf_path):
+        self.pdf_path = pdf_path
+        self.reader = PyPDF2.PdfReader(pdf_path)
+        self.writer = PyPDF2.PdfWriter()
+
+    def overlay_pdf(self, overlay_pdf_path, page_num):
+        overlay_pdf = PyPDF2.PdfReader(overlay_pdf_path)
+        overlay_page = overlay_pdf.pages[0]
+        overlay_page.merge_page(self.reader.pages[page_num])
+        self.writer.add_page(overlay_page)
+
+    def save_pdf(self, output_path):
+        with open(output_path, 'wb') as f:
+            self.writer.write(f)
