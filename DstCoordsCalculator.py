461be566124cd658c29cea610854c8310145e73c
Laptop <djm07048@gmail.com>
2024-08-13 14:09:47 +0900

    240812

--- DstCoordsCalculator.py
+++ DstCoordsCalculator.py
@@ -32,62 +32,174 @@ for cont_code, coords in row['item_cont_info'].items():
         'src_pdf_path': 'input',
         'src_pdf_page': 'input',
         'src_pdf_coords': 'input',
+        'src_pdf_height': 'input',
         'dst_pdf_path': None,
         'dst_pdf_page': None,
         'dst_pdf_para': None,
         'dst_pdf_coords': None
     }
 
-# merged_cont_df를 받아온다.
-# test_type and book_type에 따라 서로 다른 합체 방식을 구현
-# 비어있는 dst_pdf_path, dst_page, dst_coords를 채워야 함
-# src_pdf_page, src_pdf_coords, src_pdf_path는 주어져 있음
 
-# 1. 모의고사 문제지) test_type = 'ex', book_type = 'pbm'
-    '''
-    1. filter cont_type = pbm
-    
-    2. book_cont = Null
-    
-    3. dst_pdf_path
-        test_code로부터 받아온 것에 맞는 pdf file을 선택하면 됨
-        
-    4. dst_pdf_page & dst_pdf_para
-        input으로 받아야 함
-        
-    5. dst_pdf_coords
-        pre-defined para
-        for 1a ~ 4a
-            varied-spacing btw each cont
-            justify-align
-        for 4b
-            varied-spacing btw each cont
-            top-align
-            
-    6. overlay_multiple_pdf_from_merged_cont_df(merged_cont_df)
-    '''
+    dict_para_coords = {
+        '1a' : [x_lt, x_rt, y_upper, y_lower, height, dst_pdf_file, dst_pdf_page],
+        '1b' : [x_lt, x_rt, y_upper, y_lower, height, dst_pdf_file, dst_pdf_page],
+        '2a' : [x_lt, x_rt, y_upper, y_lower, height, dst_pdf_file, dst_pdf_page],
+    }
 
-# 2. 모의고사 해설지) test_type = 'ex', book_type = 'sol'
 
-    '''
-    1. filter cont_type = sol
-    
-    2. book_cont = header
-    
-    3. dst_pdf_path
-        test_code로부터 받아온 것에 맞는 pdf file을 선택하면 됨
-        유일함
-    
-    4. dst_pdf_page & dst_pdf_para
-        -> calculated
-        
-    5. dst_pdf_coords
-        un-defined para
-        fixed-min-spacing btw each cont
-        justify-align
-    
-    '''
+    # value = [alignment, spacing]
+    # alignment = 'justity_alingn', 'justify_with_last_blank', 'top_align'
+    # spacing = int, 50 for top_align
+    dict_para_alignment = {
+        '1a' : ['justify_align', 0],
+        '1b' : ['justify_align', 0],
+        '2a' : ['justify_align', 0],
+        '2b' : ['justify_align', 0],
+        '3a' : ['justify_align', 0],
+        '3b' : ['justify_align', 0],
+        '4a' : ['justify_align', 0],
+        '4b' : ['justify_with_last_blank', 0]
 
-# 3. 주간지 문제 파트) test_type = 'wk', book_type = 'pbm'
+    }
 
-# 4. 주간지 해설 파트) test_type = 'wk', book_type = 'sol'
+    para = ['1a', '1b', '2a', '2b', '3a', '3b', '4a', '4b']
+# 1) merged_cont_df를 받아오는 코드
+
+# 시작하기 전에
+# para, dict_para_coords, dict_para_alignment를 정의해야 함
+# 이때 dict들은 재사용을 전제로 구성하지 말고, 모든 page에 대해 모두 정의해주어야 함.
+# if 2단으로 100page -> 길이가 200인 para, dict를 만들어야 함
+# 물론, 이때 반복문을 활용하여 할 수도 있겠다.
+
+# book_code는 입력하면 됨
+# merged_cont_df는 열심히.. 만든 것을 받아오면 됨
+
+class DstCoordsCalculator:
+    def __init__(self, book_code, merged_cont_df, para):
+        self.merged_cont_df = merged_cont_df
+        self.para = para
+        self.book_code = book_code
+        self.book_type = book_code.split('_')[1]
+        self.test_type = book_code.split('_')[0]
+
+        self.is_para_predefined = {
+            ['ex', 'pbm']: True,
+            ['ex', 'sol']: False,
+            ['wk', 'pbm']: False,
+            ['wk', 'sol']: False
+        }
+
+    def group_by_para(self):
+        if self.is_para_predefined[self.test_type, self.book_type]:
+            # merged_cont_df에 predefined para의 정보를 넣어 주어야 한다.
+            self.para_group = self.merged_cont_df.groupby('dst_pdf_para')
+        else:
+            current_para = para[0]
+            current_y = dict_para_coords[current_para][2]
+            for index, row in self.merged_cont_df.iterrows():
+                height = row['src_pdf_height']
+                current_y -= height
+                if current_y < dict_para_coords[current_para][4]:
+                    current_para = para[para.index(current_para) + 1]
+                    current_y = dict_para_coords[current_para][2]
+                else:
+                    row['dst_pdf_para'] = current_para
+
+                row['dst_pdf_page'] = dict_para_coords[current_para][5]
+                row['dst_pdf_path'] = dict_para_coords[current_para][6]
+
+            self.para_group = self.merged_cont_df.groupby('dst_pdf_para')
+
+        # Check if the row's 'cont_type' is 'blank' and remove the row if true
+        self.merged_cont_df = self.merged_cont_df[self.merged_cont_df['cont_type'] != 'blank']
+
+        # align conts in para
+        for para, group in self.para_group:
+            alignment, spacing = dict_para_alignment[para]
+            if alignment == 'justify_align':
+                self.justify_align(group, spacing)
+            elif alignment == 'justify_with_last_blank':
+                self.justify_with_last_blank(group, spacing)
+            elif alignment == 'top_align':
+                self.top_align(group, spacing)
+
+        return self.merged_cont_df
+
+    def justify_align(self, group, spacing):
+        para = group['dst_pdf_para'].iloc[0]
+        current_y = dict_para_coords[para][2]
+
+        # Calculate total height of all rows in the group
+        total_height = group['src_pdf_height'].sum()
+
+        # Count the number of item_code changes
+        item_codes = group['item_code']
+        item_change = (item_codes != item_codes.shift()).sum() - 1
+
+        # Calculate spacing between items
+        if item_change != 0:
+            spacing_btw_item = (dict_para_coords[para][2] - dict_para_coords[para][3] - total_height) / item_change
+        else:
+            spacing_btw_item = 0
+
+        # Iterate over each row in the group
+        for index, row in group.iterrows():
+            row['dst_pdf_coords'][2] = current_y
+            current_y -= row['src_pdf_height']
+            row['dst_pdf_coords'][0] = dict_para_coords[para][0]
+            row['dst_pdf_coords'][1] = dict_para_coords[para][1]
+            row['dst_pdf_coords'][3] = current_y
+
+            # Check if the current row's item_code is different from the next row's item_code
+            if index < len(group) - 1 and row['item_code'] != group.iloc[index + 1]['item_code']:
+                current_y -= spacing_btw_item
+
+    def justify_with_last_blank(self, group, spacing):
+        para = group['dst_pdf_para'].iloc[0]
+        current_y = dict_para_coords[para][2]
+
+        # Calculate total height of all rows in the group
+        total_height = group['src_pdf_height'].sum()
+
+        # Count the number of item_code changes
+        item_codes = group['item_code']
+        item_change = (item_codes != item_codes.shift()).sum() - 1
+
+        # Calculate spacing between items
+        if item_change != 0:
+            spacing_btw_item = (dict_para_coords[para][2] - dict_para_coords[para][3] - total_height) / (item_change+1)
+        else:
+            spacing_btw_item = 0
+
+        # Iterate over each row in the group
+        for index, row in group.iterrows():
+            row['dst_pdf_coords'][2] = current_y
+            current_y -= row['src_pdf_height']
+            row['dst_pdf_coords'][0] = dict_para_coords[para][0]
+            row['dst_pdf_coords'][1] = dict_para_coords[para][1]
+            row['dst_pdf_coords'][3] = current_y
+
+            # Check if the current row's item_code is different from the next row's item_code
+            if index < len(group) - 1 and row['item_code'] != group.iloc[index + 1]['item_code']:
+                current_y -= spacing_btw_item
+
+        # Add spacing at the end of the group
+        current_y -= spacing_btw_item
+
+    def top_align(self, group, spacing):
+        para = group['dst_pdf_para'].iloc[0]
+        current_y = dict_para_coords[para][2]
+
+        # Iterate over each row in the group
+        for index, row in group.iterrows():
+            row['dst_pdf_coords'][2] = current_y
+            current_y -= row['src_pdf_height']
+            row['dst_pdf_coords'][0] = dict_para_coords[para][0]
+            row['dst_pdf_coords'][1] = dict_para_coords[para][1]
+            row['dst_pdf_coords'][3] = current_y
+
+            # Check if the current row's item_code is different from the next row's item_code
+            if index < len(group) - 1 and row['item_code'] != group.iloc[index + 1]['item_code']:
+                current_y -= spacing
+
+
